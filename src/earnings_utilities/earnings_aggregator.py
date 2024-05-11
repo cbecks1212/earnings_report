885a0263c30864aaa2256593a5eee556513ccacb
Curt Beck <curt.beck1254@gmail.com>
2024-05-10 21:45:51 -0400

    troubleshooting

--- src/earnings_utilities/earnings_aggregator.py
+++ src/earnings_utilities/earnings_aggregator.py
@@ -15,6 +15,7 @@ secret_name = f"projects/{project_id}/secrets/{secret_id}/versions/latest"
 
 # Access the secret
 response = client.access_secret_version(request={"name": secret_name})
+print(response)
 payload = response.payload.data.decode("UTF-8")
 
 class EarningsCalculator:
