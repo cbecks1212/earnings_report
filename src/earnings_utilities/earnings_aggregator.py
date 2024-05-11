42cfa73ddcbffda98614fa01f875fa3a8060e1c1
Curt Beck <curt.beck1254@gmail.com>
2024-05-10 21:19:51 -0400

    added google secrets

--- src/earnings_utilities/earnings_aggregator.py
+++ src/earnings_utilities/earnings_aggregator.py
@@ -4,11 +4,18 @@ import numpy as np
 import pandas as pd
 import requests
 #from src.config import settings
-from dotenv import load_dotenv
-import os
+from google.cloud import secretmanager
 
-load_dotenv()
-API_KEY = os.getenv("DEV_FIN_APIKEY")
+client = secretmanager.SecretManagerServiceClient()
+project_id = "orbital-kit-400022"
+secret_id = "DEV_FIN_APIKEY"
+
+# Build the secret name
+secret_name = f"projects/{project_id}/secrets/{secret_id}/versions/latest"
+
+# Access the secret
+response = client.access_secret_version(request={"name": secret_name})
+payload = response.payload.data.decode("UTF-8")
 
 class EarningsCalculator:
     def calc_earning_start_end_date(self):
@@ -38,17 +45,18 @@ class EarningsCalculator:
         start_date, end_date = EarningsCalculator().calc_earning_start_end_date()
 
         resp = requests.get(
-            f"https://financialmodelingprep.com/api/v3/earning_calendar?from={start_date}&to={end_date}&apikey={API_KEY}"
+            f"https://financialmodelingprep.com/api/v3/earning_calendar?from={start_date}&to={end_date}&apikey={payload}"
         )
+        print(resp.status_code)
 
-        #df = pd.json_normalize(resp.json())
-        """filtered_df = df.query("eps.notnull()", engine="python").query(
+        df = pd.json_normalize(resp.json())
+        filtered_df = df.query("eps.notnull()", engine="python").query(
             "epsEstimated.notnull()", engine="python"
-        )"""
+        )
         #filtered_df = df[(~df['eps'].isnull()) & (~df['epsEstimated'].isnull())]
-        """filtered_df["beatEarnings"] = np.where(
+        filtered_df["beatEarnings"] = np.where(
             filtered_df["eps"] > filtered_df["epsEstimated"], 1, 0
-        )"""
-        #summary_dict = filtered_df["beatEarnings"].value_counts().to_dict()
-        summary_dict = {"status_code" : resp.status_code}
+        )
+        summary_dict = filtered_df["beatEarnings"].value_counts().to_dict()
+        #summary_dict = {"status_code" : resp.status_code}
         return summary_dict
