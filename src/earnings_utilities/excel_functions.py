48907f7f0574aff6ff9a17509d48c59dbe386213
Curt Beck <curt.beck1254@gmail.com>
2024-06-19 17:56:32 -0400

    added earnings anaylsis excel endpoint

--- src/earnings_utilities/excel_functions.py
+++ src/earnings_utilities/excel_functions.py
@@ -1,5 +1,6 @@
 from io import BytesIO
 import openpyxl
+import openpyxl.workbook
 import pandas as pd
 
 def create_excel_object(records: dict):
@@ -22,3 +23,18 @@ def create_excel_object(records: dict):
     workbook.save(final_buffer)
 
     return final_buffer
+
+def adjust_excel_column_widths(workbook: openpyxl.Workbook) -> openpyxl.Workbook:
+    for sheetname in workbook.sheetnames:
+        worksheet = workbook[sheetname]
+
+        dims = {}
+        for row in worksheet.rows:
+            for cell in row:
+                if cell.value:
+                    dims[cell.column_letter] = max((dims.get(cell.column_letter, 0), len(str(cell.value))))
+
+        for col, value in dims.items():
+            worksheet.column_dimensions[col].width = value
+    return workbook
+
--- src/routers/earnings_documents.py
+++ src/routers/earnings_documents.py
@@ -3,9 +3,12 @@ from typing import Optional
 import requests
 import json
 from io import BytesIO
+import pandas as pd
+import openpyxl
+import requests
 from ..models.earnings_summary import EarningsSummarizer
 from ..earnings_utilities.earnings_aggregator import EarningsCalculator
-from ..earnings_utilities.excel_functions import create_excel_object
+from ..earnings_utilities.excel_functions import create_excel_object, adjust_excel_column_widths
 
 router = APIRouter()
 
@@ -46,7 +49,41 @@ def download_announcements_excel(model: Optional[EarningsSummarizer] = None):
     buffer.seek(0)
     
     headers = {
-        'Content-Disposition': 'attachment; filename="sample.xlsx"'
+        'Content-Disposition': 'attachment; filename="Earnings_Announcements.xlsx"'
     }
 
     return Response(content=buffer.getvalue(), media_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', headers=headers)
+
+@router.post("/download-earnings-summary-excel", summary="Download an analysis of the current earning season as an Excel file.")
+def download_earnings_summary_excel(model: Optional[EarningsSummarizer] = None):
+    if model is not None:
+        data = model.model_dump()
+    else:
+        data = None
+    
+    summary_data = requests.post("https://practical-hamilton-mqecybx2ra-uc.a.run.app/earnings-summary", json=data).text
+
+    breakout_data = requests.post("https://practical-hamilton-mqecybx2ra-uc.a.run.app/earnings-breakout", json=data).text
+
+    initial_buffer = BytesIO()
+    summary_df = pd.DataFrame(json.loads(summary_data))[['Beat', 'Missed']].drop_duplicates()
+    breakout_df = pd.DataFrame(json.loads(breakout_data))
+
+    with pd.ExcelWriter(initial_buffer, engine='openpyxl') as writer:
+        summary_df.to_excel(writer, sheet_name="Summary", index=False)
+        breakout_df.to_excel(writer, sheet_name="Earnings Breakout", index=False)
+
+
+    workbook = openpyxl.load_workbook(initial_buffer)
+    workbook = adjust_excel_column_widths(workbook)
+    final_buffer = BytesIO()
+    workbook.save(final_buffer)
+
+    final_buffer.seek(0)
+    
+    headers = {
+        'Content-Disposition': 'attachment; filename="Earnings_Season_Analysis.xlsx"'
+    }
+
+    return Response(content=final_buffer.getvalue(), media_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', headers=headers)
+
