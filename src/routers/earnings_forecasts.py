af48c2d465e022a1f069e5be168c39ffc47014cf
Curt Beck <curt.beck1254@gmail.com>
2024-08-04 16:19:51 -0400

    updated model caching again

--- src/routers/earnings_forecasts.py
+++ src/routers/earnings_forecasts.py
@@ -3,6 +3,7 @@ import asyncio
 import pandas as pd
 import numpy as np
 import joblib
+from functools import lru_cache
 from io import BytesIO
 from typing import Annotated
 from sqlalchemy.orm import Session
@@ -11,6 +12,8 @@ from ..database.utils import get_db
 from ..database.tables import Models
 from ..models.earnings_forecasts import EarningsForecast
 from ..earnings_utilities.earnings_forecasts_utils import MLUtils
+
+model_cache = {}
 
 
 router = APIRouter(
@@ -28,6 +31,12 @@ def load_ml_model(data, file_name, db):
 
     db.add(sql_data)
     db.commit()
+
+@lru_cache
+async def load_model(binary_data, model_name):
+    model = joblib.load(BytesIO(binary_data))
+    model_cache[model_name] = model
+    return model
 
 
 @router.get("/view-models")
@@ -60,7 +69,11 @@ async def forecast_eps(db: db_dependency, model: EarningsForecast):
     x = await asyncio.to_thread(MLUtils().load_eps_feature_data, ticker)
     model_result = await asyncio.to_thread(db.query(Models).filter(Models.model_name == 'eps_predictor_v2.pkl').first)
     binary_data = model_result.model
-    model = await asyncio.to_thread(MLUtils().load_model, binary_data)
+    if model_result.model_name in model_cache:
+        model = model_cache[model_result.model_name]
+        print("MODEL CACHED")
+    else:
+        model = await load_model(binary_data, model_result.model_name)
     #model = joblib.load(BytesIO(binary_data))
     pred = await asyncio.to_thread(model.predict, x)
     return {"Forecasted EPS" : pred[0].item()}
