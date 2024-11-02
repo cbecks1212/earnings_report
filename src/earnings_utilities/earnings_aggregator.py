0e76eb001ddbfdd4fcf3217a80fa6608e79401bf
Curt Beck <curt.beck1254@gmail.com>
2024-11-02 18:59:57 -0400

    further enhancements

--- src/earnings_utilities/earnings_aggregator.py
+++ src/earnings_utilities/earnings_aggregator.py
@@ -289,13 +289,13 @@ class EarningsCalculator:
             if ticker_df['date'].iloc[0] <= today_date:
                 ticker_df["beatEarnings"] = np.where(
                 ticker_df["eps"] > ticker_df["epsEstimated"], "Beat", "Missed")
-                ticker_df["performanceAfterEarnings"] = ticker_df.apply(lambda x: EarningsCalculator().calc_price_perf_after_earnings(x.symbol, datetime.strftime(x.date, "%Y-%m-%d")), axis=1)
-                earnings_summary_text = EarningsCalculator().generate_ai_text(symbol)
+                ticker_df["performanceAfterEarnings"] = ticker_df.apply(lambda x: earnings_agg.calc_price_perf_after_earnings(x.symbol, datetime.strftime(x.date, "%Y-%m-%d")), axis=1)
+                earnings_summary_text = earnings_agg.generate_ai_text(symbol)
                 summary_dict = ticker_df[["date", "symbol", "eps", "epsEstimated", "beatEarnings", "performanceAfterEarnings"]].to_dict(orient="records")
                 summary_dict[0].update({"transcriptSummary" : earnings_summary_text})
             else:
                 """earnings_date = datetime.strftime(ticker_df['date'].iloc[0], "%Y-%m-%d")"""
-                earnings_date = EarningsCalculator().get_earnings_announcement_symbol(symbol)["announcement_date"]
+                earnings_date = earnings_agg.get_earnings_announcement_symbol(symbol)["announcement_date"]
                 summary_dict = {"symbol" : symbol, "earnings_date": f"{symbol} will announce its earnings on {earnings_date}"}
         else:
             summary_dict = {"msg" : f"{symbol}'s earnings date has yet to be confirmed"}
