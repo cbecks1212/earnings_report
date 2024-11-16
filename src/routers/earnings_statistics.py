f6e983746e1a16f1c678554e04203a99899dd59f
Curt Beck <curt.beck1254@gmail.com>
2024-11-16 12:07:54 -0500

    made syntax adjustment to price-after-earnings endpoint

--- src/routers/earnings_statistics.py
+++ src/routers/earnings_statistics.py
@@ -49,8 +49,8 @@ async def peer_earnings_analysis(symbol: str, peer_model: Optional[PeerEarnings]
     return peer_analysis
 
 @router.get("/price-after-earnings", status_code=200)
-async def get_price_after_earnings(ticker: str):
+async def get_price_after_earnings(symbol: str):
     earnings_calc = EarningsCalculator()
-    ticker_earnings_date = earnings_calc.get_earnings_announcement_symbol(ticker)['announcement_date']
-    price_perf = EarningsCalculator().calc_price_perf_after_earnings(ticker, ticker_earnings_date)
-    return {"ticker" : ticker, "earnings_date" : ticker_earnings_date, "price_performance_after_earnings" : price_perf}
+    ticker_earnings_date = earnings_calc.get_earnings_announcement_symbol(symbol)['announcement_date']
+    price_perf = EarningsCalculator().calc_price_perf_after_earnings(symbol, ticker_earnings_date)
+    return {"symbol" : symbol, "earnings_date" : ticker_earnings_date, "price_performance_after_earnings" : price_perf}
